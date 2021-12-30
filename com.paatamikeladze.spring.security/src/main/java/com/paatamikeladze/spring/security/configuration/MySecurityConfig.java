9b61f66e9cf9d9721b39f4db54e9748365d5a170
Paata Mikeladze <granddfat@gmail.com>
2021-12-30 03:13:51 +0300

    Add features with logging&password with crypt from db

--- com.paatamikeladze.spring.security/src/main/java/com/paatamikeladze/spring/security/configuration/MySecurityConfig.java
+++ com.paatamikeladze.spring.security/src/main/java/com/paatamikeladze/spring/security/configuration/MySecurityConfig.java
@@ -1,28 +1,35 @@
 package com.paatamikeladze.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 //Помечаем класс ответственный за конфигруацию security
 //в котором пропишем данные
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //Дефолтное шифрование паролей
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        /*Метод говорит, что при проверке данных, необходимо сравнивать с данными
-        * которые мы сейчас пропишем, они будут храниться в памяти, данный метод устаревший
-        * удобнее хранить данные в БД, но сейчас мы делаем, для рассмотрения возможностей*/
-        auth.inMemoryAuthentication()
-                .withUser("paata").password("{noop}paata").roles("IT");
-        auth.inMemoryAuthentication()
-                .withUser("vera").password("{noop}vera").roles("Methodist");
-        auth.inMemoryAuthentication()
-                .withUser("alisa").password("{noop}alisa").roles("princess");
-
+        //теперь спринг знает, что информацию нужно брать из бд
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+//Запрашивается авторизация для определенных юрл, к конкретным ролям
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("IT","Methodist","princess")
+                .antMatchers("/hr_info").hasRole("princess")
+                //две звездочки значит, на любой следующий адресс в этой ветке
+                .antMatchers("/manger_info/**").hasRole("Methodist")
+                //означает, что форма логина и пароля будет запрашиваться у всех
+                .and().formLogin().permitAll();
 
     }
 }
--- com.paatamikeladze.spring.security/src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ com.paatamikeladze.spring.security/src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,16 +1,18 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!doctype html>
 <html>
 
 <body>
 <h3>Information</h3>
-
+<security:authorize access="hasRole('princess')">
 <input type="button" value="Salary"
 onclick="window.location.href='hr_info'">
 Only princess
-
+</security:authorize>
+<security:authorize access="hasRole('methodist')">
 <input type="button" value="Performance"
        onclick="window.location.href='manger_info'">
 Only Methodist
-
+</security:authorize>
 </body>
 </html>
